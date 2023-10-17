cf4c49ba76a1dc61912a1d818db73e166ce65ca0
hieptvhe173252@fpt.edu.vn <hieptvhe173252@fpt.edu.vn>
2023-10-17 22:47:44 +0700

    Add project files.

---
+++ License-Key-Shop-Web/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace License_Key_Shop_Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
